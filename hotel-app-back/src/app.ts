29e40f38076471a8f6ace591665d238e1dea7514
Tyrant09 <bosskung0880@gmail.com>
2026-03-09 18:43:07 +0700

    fix(app): fix console.log(/api/v1 => /api)

--- hotel-app-back/src/app.ts
+++ hotel-app-back/src/app.ts
@@ -9,5 +9,5 @@ app.use(express.json());
 app.use("/api/", apiRoutes);
 
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}/api/v1`);
+  console.log(`App listening at http://localhost:${port}/api`);
 });
