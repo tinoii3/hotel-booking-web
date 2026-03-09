86908047c3925d8c428f53ec9fa7a222f772dfa1
Tyrant09 <bosskung0880@gmail.com>
2026-03-09 18:35:15 +0700

    feat(manage-room.repository): add roomTypeFindAll, roomTypeCreate, roomCreate, roomUpdate, roomDelete

--- hotel-app-back/src/repository/manage-room.repository.ts
+++ hotel-app-back/src/repository/manage-room.repository.ts
@@ -3,3 +3,32 @@ import { prisma } from "../lib/prisma.js"
 export const roomFindAll = async () => {
     return prisma.rooms.findMany();
 }
+
+export const roomTypeFindAll = async () => {
+    return prisma.room_types.findMany();
+}
+
+export const roomCreate = async (data: any) => {
+    return prisma.rooms.create({
+        data: data
+    });
+}
+
+export const roomTypeCreate = async (data: any) => {
+    return prisma.room_types.create({
+        data: data
+    });
+}
+
+export const roomUpdate = async (id: string, data: any) => {
+    return prisma.rooms.update({
+        where: {id: id},
+        data: data
+    });
+}
+
+export const roomDelete = async (id: string) => {
+    return prisma.rooms.delete({
+        were: {id: id}
+    });
+}
