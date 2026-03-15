6a7e5c6fbfe569681a7c39d1b1e27c1778b80b4b
tinoii3 <kritsanai.bun@gmail.com>
2026-03-15 15:30:27 +0700

    feat(watcher): error handling global, bareer request

--- hotel-app-front/src/app/app.config.ts
+++ hotel-app-front/src/app/app.config.ts
@@ -3,12 +3,14 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
-import { loadingInterceptor } from './interceptors/loading.interceptor';
+import { loadingInterceptor } from './interceptors/loading/loading-interceptor';
+import { errorInterceptor } from './interceptors/error/error-interceptor';
+import { authInterceptor } from './interceptors/auth/auth-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideHttpClient(withInterceptors([loadingInterceptor])),
+    provideHttpClient(withInterceptors([loadingInterceptor, errorInterceptor, authInterceptor])),
     provideRouter(routes),
   ],
 };
