d4fa8650ced66b5b954fd3553c30f5f2e050baac
tinoii3 <kritsanai.bun@gmail.com>
2026-03-19 20:56:54 +0700

    feat(constant): add new value constant

--- hotel-app-front/src/app/core/constants/search.constants.ts
+++ hotel-app-front/src/app/core/constants/search.constants.ts
@@ -1,8 +1,10 @@
 export const ROOM_TYPES = [
-  { label: 'ทุกประเภทห้องพัก', value: 'ALL' },
-  { label: 'ห้อง Deluxe', value: 'DELUXE' },
-  { label: 'ห้อง Suite', value: 'SUITE' },
-  { label: 'ห้อง Presidential', value: 'PRESIDENTIAL' },
+  { label: 'ทุกประเภทห้องพัก', value: 'all' },
+  { label: 'ห้องมาตรฐาน', value: 'standard' },
+  { label: 'ห้องดีลักซ์', value: 'deluxe' },
+  { label: 'ห้องสูท', value: 'suite' },
+  { label: 'ห้องสุดพิเศษ', value: 'superior' },
+  { label: 'ห้องสตูดิโอ', value: 'studio' },
 ];
 
 export const GUEST_OPTIONS = [
