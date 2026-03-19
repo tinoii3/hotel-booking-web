c11cde15c6a4c93116d84b8b1aaa5a0680309358
Tyrant09 <bosskung0880@gmail.com>
2026-03-19 16:41:24 +0700

    fix(admin-layout-component.html, admin-layout-component.ts):fix header routetitles

--- hotel-app-front/src/app/layouts/admin/admin-layout-component/admin-layout-component.ts
+++ hotel-app-front/src/app/layouts/admin/admin-layout-component/admin-layout-component.ts
@@ -1,8 +1,9 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet, RouterModule, Router } from '@angular/router';
+import { RouterOutlet, RouterModule, Router, NavigationEnd } from '@angular/router';
 import { AuthService } from '../../../core/services/auth-service';
 import Swal from 'sweetalert2';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-admin-layout-component',
@@ -14,6 +15,29 @@ import Swal from 'sweetalert2';
 export class AdminLayout {
   private authService = inject(AuthService);
   private router = inject(Router);
+
+  pageTitle: string = 'Dashboard';
+
+  private routeTitles: { [key: string]: string } = {
+    '/admin/dashboard': 'Dashboard',
+    '/admin/manage-room': 'จัดการห้องพัก',
+    '/admin/reserv-room': 'การจอง',
+    '/admin/manage-staff': 'การจัดการผู้ดูแล'
+  };
+
+  ngOnInit() {
+    this.updateTitle(this.router.url);
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: any) => {
+      this.updateTitle(event.urlAfterRedirects);
+    });
+  }
+
+  private updateTitle(url: string) {
+    const path = url.split('?')[0];
+    this.pageTitle = this.routeTitles[path] || 'Hotel Admin';
+  }
 
   logout() {
     Swal.fire({
