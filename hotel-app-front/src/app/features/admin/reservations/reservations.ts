e2fc39b1aff867ac241e2767bacf95e7f0bd1988
Napat Pitaktum <pitaktum.napat@gmail.com>
2026-03-21 00:18:23 +0700

    change style filter btn

--- hotel-app-front/src/app/features/admin/reservations/reservations.ts
+++ hotel-app-front/src/app/features/admin/reservations/reservations.ts
@@ -179,8 +179,11 @@ export class Reservation implements OnInit {
       showCancelButton: true,
       confirmButtonText: 'ยืนยัน',
       cancelButtonText: 'ยกเลิก',
-      confirmButtonColor: '#0d6efd',
-      cancelButtonColor: '#6c757d'
+      customClass: {
+        cancelButton: 'btn-dialog btn-dialog-secondary',
+        confirmButton: 'btn-dialog btn-dialog-primary',
+      },
+      reverseButtons: true
     }).then((result) => {
       if (result.isConfirmed && result.value) {
         this.updateReservationStatus(reservation.id, result.value);
